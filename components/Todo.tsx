961d2b16ad9239acad5e5b2ee213ff2861b4a9d1
Bakare Abiola <iamstarcode@gmail.com>
2023-12-29 21:11:32 +0100

    feat: create todo component

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -8,59 +8,61 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 import { CardContent, Card } from '@/components/ui/card';
+import { createClient } from '@/utils/supabase/client';
+import { useEffect, useState } from 'react';
 
 export default function Component() {
+  const supabase = createClient();
+  const [todos, setTodos] = useState<any[] | null>([]);
+
+  useEffect(() => {
+    async function getTodos() {
+      const todos = await supabase.from('todos').select('*');
+      setTodos(todos.data);
+    }
+
+    getTodos();
+  }, []);
+
+  console.log(todos);
   return (
     <div className='p-4 space-y-4'>
       <h1 className='text-2xl font-bold'>Todo List</h1>
-      <Card>
-        <CardContent className='flex flex-row items-start gap-2 p-4'>
-          <div className='space-y-2 leading-none'>
-            <div className='flex items-center space-x-2'>
-              <Checkbox defaultChecked id='todo1' />
-              <Label htmlFor='todo1'>Todo Item 1</Label>
-            </div>
-            <p className='text-sm text-gray-500 dark:text-gray-400'>
-              Created: 29th December 2023
-            </p>
-          </div>
-          <Button className='ml-auto' variant='outline'>
-            Delete
-          </Button>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardContent className='flex flex-row items-start gap-2 p-4'>
-          <div className='space-y-2 leading-none'>
-            <div className='flex items-center space-x-2'>
-              <Checkbox id='todo2' />
-              <Label htmlFor='todo2'>Todo Item 2</Label>
-            </div>
-            <p className='text-sm text-gray-500 dark:text-gray-400'>
-              Created: 28th December 2023
-            </p>
-          </div>
-          <Button className='ml-auto' variant='outline'>
-            Delete
-          </Button>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardContent className='flex flex-row items-start gap-2 p-4'>
-          <div className='space-y-2 leading-none'>
-            <div className='flex items-center space-x-2'>
-              <Checkbox id='todo3' />
-              <Label htmlFor='todo3'>Todo Item 3</Label>
-            </div>
-            <p className='text-sm text-gray-500 dark:text-gray-400'>
-              Created: 27th December 2023
-            </p>
-          </div>
-          <Button className='ml-auto' variant='outline'>
-            Delete
-          </Button>
-        </CardContent>
-      </Card>
+      {todos !== null ? (
+        todos.map((todo: any) => (
+          <>
+            <Card id={todo.id}>
+              <CardContent className='flex flex-row items-start gap-2 p-4'>
+                <div className='space-y-2 leading-none'>
+                  <div className='flex items-center space-x-2'>
+                    <Checkbox id='todo1' className='peer' />
+                    <Label
+                      htmlFor='todo1'
+                      className='text-lg text-foreground/90 peer-checked:line-through font-semibold hover:cursor-pointer'
+                    >
+                      {todo.description}
+                    </Label>
+                  </div>
+                  <p className='text-sm text-gray-500 dark:text-gray-400'>
+                    {new Intl.DateTimeFormat('en-US', {
+                      year: 'numeric',
+                      month: 'long',
+                      day: 'numeric',
+                      hour: 'numeric',
+                      minute: 'numeric',
+                    }).format(new Date(todo.created_at))}
+                  </p>
+                </div>
+                <Button className='ml-auto' variant='outline'>
+                  Delete
+                </Button>
+              </CardContent>
+            </Card>
+          </>
+        ))
+      ) : (
+        <p>No todos</p>
+      )}
     </div>
   );
 }
--- todo.sql
+++ todo.sql
@@ -8,3 +8,7 @@ CREATE TABLE IF NOT EXISTS todos (
 );
 
 ALTER TABLE todos ENABLE ROW LEVEL SECURITY;
+
+
+INSERT INTO todos (user_id, description, is_completed) VALUES ('2bf1f27c-b65f-4c8e-b2fc-77d916e20e56', 'Finish task 2', TRUE);
+
